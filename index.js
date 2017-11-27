1ba75bff84b915f005231522787ba67046501beb
Trevor Blades <tdblades@gmail.com>
2017-11-27 01:29:19 -0800

    Use get-image-colors

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const Color = require('color');
-const ColorThief = require('color-thief');
+const fileType = require('file-type');
+const getColors = require('get-image-colors');
 const loaderUtils = require('loader-utils');
 
-const rgbString = color => Color.rgb(color).toString();
-
-// eslint-disable-next-line func-names
-module.exports = function(content) {
-  const colorThief = new ColorThief();
-  const colors = colorThief.getPalette(content);
-  const options = loaderUtils.getOptions(this);
-  const result = JSON.stringify(
-    options && options.simple ? rgbString(colors[0]) : colors.map(rgbString)
-  );
-  return `module.exports = ${result};`;
+module.exports = content => {
+  const {mime} = fileType(content);
+  return getColors(content, mime).then(colors => {
+    const options = loaderUtils.getOptions(this);
+    const result = JSON.stringify(
+      options && options.simple
+        ? colors[0].hex()
+        : colors.map(color => color.hex())
+    );
+    return `module.exports = ${result};`;
+  });
 };
 module.exports.raw = true;
